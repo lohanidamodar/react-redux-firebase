03ec9f61e2ccdf3052347c3790262710c1d3e578
Damodar Lohani <dlohani48@gmail.com>
2019-02-13 21:31:02 +0545

    dummy data on reducar and outputting

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProjectSummary = (props) => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title</span>
+            <span className="card-title">{project.title}</span>
             <p>Posted by Damodar Lohani</p>
             <p className="grey-text">1st January, 5pm</p>
         </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: 1, title: 'Help me find this', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque labore autem at architecto? Architecto cupiditate, animi sunt consectetur sequi quibusdam deleniti quod labore tenetur neque placeat eum illum minus sit!'  },
+        {id: 2, title: 'Create final', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque labore autem at architecto? Architecto cupiditate, animi sunt consectetur sequi quibusdam deleniti quod labore tenetur neque placeat eum illum minus sit!'  },
+        {id: 3, title: 'Organize that', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque labore autem at architecto? Architecto cupiditate, animi sunt consectetur sequi quibusdam deleniti quod labore tenetur neque placeat eum illum minus sit!'  },
+    ]
+}
 
 const projectReducer = (state = initState,action) => {
     return state;
