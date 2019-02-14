afcbb5d17f492779a1341d24b68b3e47146d96c6
Damodar Lohani <dlohani48@gmail.com>
2019-02-14 09:30:39 +0545

    authentication guard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,11 +4,13 @@ import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
+import { Redirect } from 'react-router-dom';
 
 export class Dashboard extends Component {
 
   render() {
-    const {projects} = this.props;
+    const {projects, auth} = this.props;
+    if(!auth.uid) return <Redirect to="/signin" />
     return (
       <div className="dashboard container">
         <div className="row">
@@ -27,7 +29,8 @@ export class Dashboard extends Component {
 const mapStateToProps = state => {
   console.log(state);
   return {
-    projects: state.firestore.ordered.projects
+    projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth
   }
 }
 
