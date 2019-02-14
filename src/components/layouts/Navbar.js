a873901fdaf2d1c102e248915e3709fb74d6cbbf
Damodar Lohani <dlohani48@gmail.com>
2019-02-14 07:29:31 +0545

    use auth state to view different links

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
   return (
     <nav className="nav-wrapper darken-3">
         <div className="container">
             <Link to='/' className="brand-logo">PB Project Manager</Link>
-            <SignedInLinks />
-            <SignedOutLinks />
+            {auth.uid
+              ? <SignedInLinks />
+              : <SignedOutLinks /> }
         </div>
     </nav>
   )
 }
 
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  }
+}
 
-export default Navbar;
+
+
+export default connect(mapStateToProps) (Navbar);
