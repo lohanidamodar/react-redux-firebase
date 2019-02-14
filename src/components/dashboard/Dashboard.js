6817fc462f6ce079afade589ccf3450e947a0783
Damodar Lohani <dlohani48@gmail.com>
2019-02-14 16:50:20 +0545

    ordering documents retrieving from firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -38,7 +38,7 @@ const mapStateToProps = state => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    {collection: 'projects'},
-    {collection: 'notifications'}
+    {collection: 'projects', orderBy: ['created_at', 'desc']},
+    {collection: 'notifications', limit: 5, orderBy: ['time','desc']}
   ])
 )(Dashboard)
