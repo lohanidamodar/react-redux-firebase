b6a83354fbcf638fb5692e0f2757078de514acc9
Damodar Lohani <dlohani48@gmail.com>
2019-02-14 16:35:44 +0545

    create notification on project create

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
-const Notifications = () => {
+const Notifications = props => {
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            {props.notifications && props.notifications.map(notification=><li>{notification.content} by {notification.user} {moment(notification.time.toDate()).calendar()}</li>)}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
