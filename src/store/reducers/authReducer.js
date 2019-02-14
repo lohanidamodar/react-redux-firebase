9d7380fc6aa62b1220a72f613684ac8ce2b4dfc7
Damodar Lohani <dlohani48@gmail.com>
2019-02-14 07:11:49 +0545

    firebase auth setup

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,21 @@
-const initState = {}
+const initState = {
+    error: null
+}
 
 const authReducer = (state = initState,action) => {
-    return state;
+    switch(action.type) {
+        case 'LOGIN_SUCCESS':
+            console.log('login success');
+            return {...state, error: null};
+            break;
+            case 'LOGIN_ERROR':
+            console.log('login failed');
+            return {...state, error: 'Login failed'};
+            break;
+        default:
+            return state;
+            break;
+    }
 }
 
 export default authReducer;
